2ac18a7855c24db0dc91c16242fef7bce02584c4
okash1n <info@okash1n.com>
2018-07-21 22:37:44 +0900

    first commit

---
+++ git-initialize.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git config --global user.name "Shintaro Okamura"
+git config --global user.email "info@okash1n.com"
+git config --global fetch.prune true
+git config --global core.editor 'nvim -c "set fenc=utf-8"'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
